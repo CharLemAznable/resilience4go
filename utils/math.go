01a5bc21ec69ab654e91471eb9d48c2982ec7830
CharLemAznable <545541819@qq.com>
2023-11-01 19:02:10 +0800

    Fixed math bug.

--- utils/math.go
+++ utils/math.go
@@ -7,6 +7,16 @@ func Min(x, y int64) int64 {
 	return x
 }
 
+func Abs(x int64) int64 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 func DivCeil(x, y int64) int64 {
-	return (x + y - 1) / y
+	if (x < 0 && y > 0) || (x > 0 && y < 0) {
+		return -(Abs(x) + Abs(y) - 1) / Abs(y)
+	}
+	return (Abs(x) + Abs(y) - 1) / Abs(y)
 }
